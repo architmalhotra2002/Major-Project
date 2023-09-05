59aea7fab5af050fab9b497ee70e9fc571143227
Aishwarya <shankeraishwarya03@gmail.com>
2023-09-05 11:12:50 +0530

    Intial commit changes

--- sentiment_analysis/yt_extractor.py
+++ sentiment_analysis/yt_extractor.py
@@ -1,37 +1,25 @@
-# Sentiment Analysis on iPhone reviews from youtube
-# Learning: youtube_dl, sentiment classification feature
-# https://youtu.be/e-kSGNzu0hM
 import youtube_dl
-from youtube_dl.utils import DownloadError
 
 ydl = youtube_dl.YoutubeDL()
 
-def get_video_info(url):
+def get_video_infos(url):
     with ydl:
-        try:
-            result = ydl.extract_info(
+        result = ydl.extract_info(
                 url,
-                download=False
+                download = False
             )
-        except DownloadError:
-            return None
-
-    if 'entries' in result:
-        # Can be a playlist or a list of videos
-        video = result['entries'][0]
-    else:
-        # Just a video
-        video = result
-    return video
-
-
-def get_audio_url(video):
-    for f in video['formats']:
-        if f['ext'] == 'm4a':
-            return f['url']
     
+    if "entries" in result:
+        # Can be a playlist or a list of videos
+        return result["entries"][0]
+    return result
 
-if __name__ == '__main__':
-    video_info = get_video_info("https://youtu.be/e-kSGNzu0hM")
-    url = get_audio_url(video_info)
-    print(url)
+def get_audio_url(video_info):
+    for f in video_info["formats"]:
+        if f["ext"] == "m4a": 
+            return f["url"]
+  
+if __name__ == "__main__":
+    video_info = get_video_infos("https://www.youtube.com/watch?v=e-kSGNzu0hM")
+    audio_url = get_audio_url(video_info)
+    print(audio_url)
