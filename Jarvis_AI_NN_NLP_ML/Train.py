88186634b25e0ce60dd2db670628c5634a915866
Archit <architmalhotra2002@gmail.com>
2023-09-24 00:23:36 +0530

    NN

--- Jarvis_AI_NN_NLP_ML/Train.py
+++ Jarvis_AI_NN_NLP_ML/Train.py
@@ -22,5 +22,51 @@ for intent in intents['intents']:
         w = tokenize(pattern)
         all_words.extend(w)
         xy.append((w,tag))
+ignore_words = [',','?','/','.','!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+x_train = []
+y_train = []
+
+for (pattern_sentence,tag) in xy:
+    bag = bag_of_words(pattern_sentence,all_words)
+    x_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(x_train[0])
+hidden_size = 8
+output_size = len(tags)
+
+print("Training The Model......")
+class ChatDataset(dataset):
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data = x_train
+        self.y_data = y_train
+    def __getintem__(self,index):
+        return self.x_data[index],self.y_data[index]
+    def __len__(self):
+        return self.n_samples
+    
+dataset = ChatDataset()
+train_loader = dataloader(dataset = dataset,
+                          batch_size = batch_size,
+                          shuffle = True,
+                          num_workers = 0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size,hidden_size,output_size).to(device = device)
+criterian = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
+
 
 
