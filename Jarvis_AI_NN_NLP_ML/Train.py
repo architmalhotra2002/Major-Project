a5e18e50849923b679826fbcc37d78c09e702cb6
Archit <architmalhotra2002@gmail.com>
2023-09-23 12:29:05 +0530

    AI Assistant

---
+++ Jarvis_AI_NN_NLP_ML/Train.py
@@ -0,0 +1,26 @@
+import numpy as np 
+import json
+import torch 
+import torch.nn as nn 
+from  torch.utils.data import dataset,dataloader
+from  NeuralNetwork import bag_of_words , tokenize , stem
+from Brain import NeuralNet
+
+with open('intents.json','r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+
+    for pattern in intent['patterns']:
+        print(pattern)
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w,tag))
+
+
