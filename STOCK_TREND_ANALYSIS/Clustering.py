fa1627e81d83a389d4737e58151ebf5c3fc52b8d
Yashika <yyaasshhiikkaa.0103@gmail.com>
2024-02-08 23:33:18 +0530

    Clustering.py commit2

--- STOCK_TREND_ANALYSIS/Clustering.py
+++ STOCK_TREND_ANALYSIS/Clustering.py
@@ -1,52 +1,35 @@
-import pandas_datareader as web
+import yfinance as yf
 import pandas as pd
 import numpy as np
 import datetime as dt
 from yahoo_fin import stock_info as si
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import make_pipeline
 from sklearn.cluster import KMeans
 
-# Get Dow Jones 30 companies
-companies_dow30 = si.tickers_dow()
+company_tickers = ['AAPL', 'NVDA', 'TSLA',  'ABBV', 'MCD', 'CCL', 'MSFT', 'GS', 'JPM' ]
 
 clusters = 5
 
-start = dt.datetime.now() - dt.timedelta(days=365 * 2)
+start = dt.datetime.now() - dt.timedelta(days=365*2)
 end = dt.datetime.now()
 
-# Create an empty DataFrame to store stock data
-data = pd.DataFrame(columns=companies_dow30)
+data = yf.download(list(company_tickers), start=start, end=end)
 
-# Fetch data for Dow Jones 30 companies
-for ticker in companies_dow30:
-    try:
-        ticker_data = web.DataReader(ticker, 'yahoo', start, end)
-        data[ticker] = ticker_data['Close']
-    except Exception as e:
-        print(f"Failed to retrieve data for {ticker}: {str(e)}")
+open_values = np.array(data['Open'].T)
+close_values = np.array(data['Close'].T)
+daily_movements = close_values - open_values
 
-# Drop any rows with missing values (NaN)
-data.dropna(inplace=True)
+normalizer = Normalizer()
+clustering_model = KMeans(n_clusters=clusters, max_iter=1000)
+pipeline = make_pipeline(normalizer, clustering_model)
+pipeline.fit(daily_movements)
+clusters = pipeline.predict(daily_movements)
 
-# Calculate daily percentage changes
-percentage_changes = data.pct_change().fillna(0)
-
-# Standardize data
-scaler = StandardScaler()
-percentage_changes_scaled = scaler.fit_transform(percentage_changes)
-
-# KMeans Clustering
-clustering_model = KMeans(n_clusters=clusters, max_iter=1000, random_state=42)
-clusters = clustering_model.fit_predict(percentage_changes_scaled)
-
-# Add clustering results to DataFrame
 results = pd.DataFrame({
-    'tickers': companies_dow30,
-    'clusters': clusters
-})
-
-# Sort DataFrame by cluster labels
-results = results.sort_values(by=['clusters'], axis=0)
+    'clusters': clusters,
+    'tickers': list(company_tickers)
+}).sort_values(by=['clusters'], axis=0)
 
 print(results)
